cf20b44d4996fee32e50bcb781920ffb4dd2f107
Robert Bevis <lawweapons@gmail.com>
2026-04-25 08:57:47 -0500

    Add type declarations for pdf-parse deep import

---
+++ lib/parsers/pdf-parse.d.ts
@@ -0,0 +1,12 @@
+declare module "pdf-parse/lib/pdf-parse.js" {
+  interface PdfParseResult {
+    text: string;
+    numpages: number;
+    numrender: number;
+    info: Record<string, unknown>;
+    metadata: unknown;
+    version: string;
+  }
+  function pdfParse(buffer: Buffer | Uint8Array, options?: unknown): Promise<PdfParseResult>;
+  export default pdfParse;
+}
